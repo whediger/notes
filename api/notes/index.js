bf895eb9e61410656db89e390df2ff40485086f5
Wes Hediger <wes.hediger@gmail.com>
2016-10-17 19:02:27 -0600

    add server

--- api/notes/index.js
+++ api/notes/index.js
@@ -1,3 +1,17 @@
-var express - require('express');
+var express = require('express');
 var app = express();
 var fs = require('fs');
+
+
+app.get('/', function(req, res){
+  fs.readFile('notes.json', 'utf8', function (err, data){
+      console.log(data);
+      res.end(data);
+  });
+});
+
+
+var server =  app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+});
